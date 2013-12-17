6bb5c182334fd5322c7fb7b712b1089c412784a2
Mark Riedesel <mark@klowner.com>
2013-12-17 08:29:50 -0600

    Initial commit

---
+++ applytransform.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+
+import sys
+sys.path.append('/usr/share/inkscape/extensions')
+import re
+
+import inkex
+import cubicsuperpath
+from simpletransform import composeTransform, fuseTransform, parseTransform, applyTransformToPath, formatTransform
+
+
+class ApplyTransform(inkex.Effect):
+    def __init__(self):
+        inkex.Effect.__init__(self)
+        self.OptionParser.add_option("-s", "--selectedonly",
+                action="store", type="inkbool",
+                dest="selectedonly", default=False,
+                help="Apply transforms to shapes")
+
+    def effect(self):
+        """
+        if self.options.selectedonly:
+            self.transformSelected(self.document, selected)
+        else:
+            self.transformAll(self.document)
+        """
+        self.recursiveFuseTransform(self.document.getroot(), parseTransform(None))
+
+
+    @staticmethod
+    def objectToPath(node):
+        if node.tag == inkex.addNS('g', 'svg'):
+            return node
+
+        if node.tag == inkex.addNS('path', 'svg') or node.tag == 'path':
+            for attName in node.attrib.keys():
+                if ("sodipodi" in attName) or ("inkscape" in attName):
+                    del node.attrib[attName]
+            return node
+
+        return node
+
+    @staticmethod
+    def recursiveFuseTransform(node, transf=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]):
+        transf = composeTransform(transf, parseTransform(node.get("transform", None)))
+
+        if 'transform' in node.attrib:
+            del node.attrib['transform']
+
+        node = ApplyTransform.objectToPath(node)
+
+        if 'd' in node.attrib:
+            d = node.get('d')
+            p = cubicsuperpath.parsePath(d)
+            applyTransformToPath(transf, p)
+            node.set('d', cubicsuperpath.formatPath(p))
+
+        elif node.tag == inkex.addNS('rect', 'svg'):
+            node.set('transform', formatTransform(transf))
+
+        for child in node.getchildren():
+            ApplyTransform.recursiveFuseTransform(child, transf)
+
+
+if __name__ == '__main__':
+    e = ApplyTransform()
+    e.affect()
+
