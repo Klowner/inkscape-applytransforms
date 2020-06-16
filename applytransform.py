b48a7b261bc0aaa584b994951969bb3868e7478f
Sillyfrog <sillyfrog@users.noreply.github.com>
2020-06-17 09:14:23 +1000

    Support for basic circle and ellipse transforms

--- applytransform.py
+++ applytransform.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 # License: GPL2
 # Copyright Mark "Klowner" Riedesel
@@ -7,10 +7,14 @@
 import sys
 sys.path.append('/usr/share/inkscape/extensions')
 
-import inkex, math
+import inkex
+import math
 from inkex.paths import CubicSuperPath, Path
 from inkex.transforms import Transform
 from inkex.styles import Style
+
+NULL_TRANSFORM = Transform([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+
 
 class ApplyTransform(inkex.Effect):
     def __init__(self):
@@ -46,7 +50,7 @@ class ApplyTransform(inkex.Effect):
 
             if 'stroke-width' in style:
                 try:
-                    stroke_width = float(style.get('stroke-width').strip())
+                    stroke_width = float(style.get('stroke-width').strip().replace("px", ""))
                     stroke_width *= math.sqrt(abs(transf.a * transf.d))
                     style['stroke-width'] = str(stroke_width)
                     update = True
@@ -57,6 +61,7 @@ class ApplyTransform(inkex.Effect):
                 node.attrib['style'] = Style(style).to_str()
 
     def recursiveFuseTransform(self, node, transf=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]):
+
         transf = Transform(transf) * Transform(node.get("transform", None))
 
         if 'transform' in node.attrib:
@@ -64,7 +69,11 @@ class ApplyTransform(inkex.Effect):
 
         node = ApplyTransform.objectToPath(node)
 
-        if 'd' in node.attrib:
+        if transf == NULL_TRANSFORM:
+            # Don't do anything if there is effectively no transform applied
+            # reduces alerts for unsupported nodes
+            pass
+        elif 'd' in node.attrib:
             d = node.get('d')
             p = CubicSuperPath(d)
             p = Path(p).to_absolute().transform(transf, True)
@@ -76,12 +85,12 @@ class ApplyTransform(inkex.Effect):
                           inkex.addNS('polyline', 'svg')]:
             points = node.get('points')
             points = points.strip().split(' ')
-            for k,p in enumerate(points):
+            for k, p in enumerate(points):
                 if ',' in p:
                     p = p.split(',')
-                    p = [float(p[0]),float(p[1])]
-                    applyTransformToPoint(transf, p)
-                    p = [str(p[0]),str(p[1])]
+                    p = [float(p[0]), float(p[1])]
+                    p = transf.apply_to_point(p)
+                    p = [str(p[0]), str(p[1])]
                     p = ','.join(p)
                     points[k] = p
             points = ' '.join(points)
@@ -89,13 +98,60 @@ class ApplyTransform(inkex.Effect):
 
             self.scaleStrokeWidth(node, transf)
 
+        elif node.tag in [inkex.addNS("ellipse", "svg"), inkex.addNS("circle", "svg")]:
+
+            def isequal(a, b):
+                return abs(a - b) <= transf.absolute_tolerance
+
+            if node.TAG == "ellipse":
+                rx = float(node.get("rx"))
+                ry = float(node.get("ry"))
+            else:
+                rx = float(node.get("r"))
+                ry = rx
+
+            cx = float(node.get("cx"))
+            cy = float(node.get("cy"))
+            sqxy1 = (cx - rx, cy - ry)
+            sqxy2 = (cx + rx, cy - ry)
+            sqxy3 = (cx + rx, cy + ry)
+            newxy1 = transf.apply_to_point(sqxy1)
+            newxy2 = transf.apply_to_point(sqxy2)
+            newxy3 = transf.apply_to_point(sqxy3)
+
+            node.set("cx", (newxy1[0] + newxy3[0]) / 2)
+            node.set("cy", (newxy1[1] + newxy3[1]) / 2)
+            edgex = math.sqrt(
+                abs(newxy1[0] - newxy2[0]) ** 2 + abs(newxy1[1] - newxy2[1]) ** 2
+            )
+            edgey = math.sqrt(
+                abs(newxy2[0] - newxy3[0]) ** 2 + abs(newxy2[1] - newxy3[1]) ** 2
+            )
+
+            if not isequal(edgex, edgey) and (
+                node.TAG == "circle"
+                or not isequal(newxy2[0], newxy3[0])
+                or not isequal(newxy1[1] != newxy2[1])
+            ):
+                inkex.utils.errormsg(
+                    "Warning: Shape %s (%s) is approximate only, try Object to path first for better results"
+                    % (node.TAG, node.get("id"))
+                )
+
+            if node.TAG == "ellipse":
+                node.set("rx", edgex / 2)
+                node.set("ry", edgey / 2)
+            else:
+                node.set("r", edgex / 2)
+
         elif node.tag in [inkex.addNS('rect', 'svg'),
                           inkex.addNS('text', 'svg'),
                           inkex.addNS('image', 'svg'),
-                          inkex.addNS('use', 'svg'),
-                          inkex.addNS('circle', 'svg')]:
-            # node.set('transform', str(Transform(transf)))
-            inkex.utils.errormsg("Shape %s not yet supported" % node.tag)
+                          inkex.addNS('use', 'svg')]:
+            inkex.utils.errormsg(
+                "Shape %s (%s) not yet supported, try Object to path first"
+                % (node.TAG, node.get("id"))
+            )
 
         else:
             # e.g. <g style="...">
